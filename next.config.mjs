6c55ff8d06bf9a29c5556d4084250e42aa419073
Pdimitris <d.panurgias@gmail.com>
2025-10-23 20:45:38 +0300

    V14

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
   },
   experimental: {
     serverComponentsExternalPackages: ["nodemailer"],
