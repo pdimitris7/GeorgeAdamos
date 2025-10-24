a72a90fa50661acaae5e6326aca352d6f1b1a6f4
Pdimitris <d.panurgias@gmail.com>
2025-10-24 16:56:39 +0300

    V21

--- app/prints/layout.tsx
+++ app/prints/layout.tsx
@@ -1,15 +1,8 @@
 // app/prints/layout.tsx
 export default function PrintsLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
+  return <>{children}</>;
 }
